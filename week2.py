15999de881b75892b998d42db4c85e2b68503e6c
Saucedo <ASaucedo44@cps.k12.il.us>
2024-09-04 15:06:16 -0500

    string slicing

--- week2.py
+++ week2.py
@@ -1,12 +1,15 @@
-# ask someone what day it is
-date =input("What day is it?")
-print(" The day is " + date)
+# # ask someone what day it is
+# date =input("What day is it?")
+# print(" The day is " + date)
 
-# ask them whats their favorite holiday
-holiday =input("What your favorite holiday?")
-print("Your favorite holiday is " + holiday)
+# # ask them whats their favorite holiday
+# holiday =input("What your favorite holiday?")
+# print("Your favorite holiday is " + holiday)
 
-# print out the day and their favorite holiday in a sentence
-print("The day is " + date + ", and your favorite holiday is " + holiday + ".")
+# # print out the day and their favorite holiday in a sentence
+# print("The day is " + date + ", and your favorite holiday is " + holiday + ".")
 
 #making a change to a file
+# string slicing
+word = "hello"
+print(word[1:4])
